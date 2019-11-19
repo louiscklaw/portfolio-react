7723dac81b471622d976f368fc2d9ac4c9ecf086
louiscklaw <louiscklaw@gmail.com>
2019-11-19 18:28:41 +0800

    update src,

--- src/components/ProjectTile.js
+++ src/components/ProjectTile.js
@@ -4,7 +4,8 @@ import {Link} from 'react-router-dom'
 import './ProjectTile.css'
 
 export default (props) => (
-    <div className="project-tile">
+    <div className="project-tile"
+      style={{backgroundImage: `url(${props.proj_image})`}}>
       <p>{props.project_name}</p>
       <Link to="/project_detail/project1">to project detail</Link>
     </div>
--- src/pages/ProjectList.js
+++ src/pages/ProjectList.js
@@ -8,52 +8,85 @@ export default () => (
   <div className="project-list-container">
     <ul>
       <li>
-        <ProjectTile project_name="project1" />
+        <ProjectTile
+          project_name="project1"
+          proj_image="https://via.placeholder.com/400x200.png"
+          />
       </li>
       <li>
-        <ProjectTile project_name="project2" />
+        <ProjectTile project_name="project2"
+        proj_image="https://via.placeholder.com/400x200.png"
+        />
       </li>
       <li>
-        <ProjectTile project_name="project3" />
+        <ProjectTile project_name="project3"
+        proj_image="https://via.placeholder.com/400x200.png"
+        />
       </li>
       <li>
-        <ProjectTile project_name="project4" />
+        <ProjectTile project_name="project4"
+        proj_image="https://via.placeholder.com/400x200.png"
+        />
       </li>
       <li>
-        <ProjectTile project_name="project5" />
+        <ProjectTile project_name="project5"
+        proj_image="https://via.placeholder.com/400x200.png"
+        />
       </li>
       <li>
-        <ProjectTile project_name="project6" />
+        <ProjectTile project_name="project6"
+        proj_image="https://via.placeholder.com/400x200.png"
+        />
       </li>
       <li>
-        <ProjectTile project_name="project7" />
+        <ProjectTile project_name="project7"
+        proj_image="https://via.placeholder.com/400x200.png"
+        />
       </li>
       <li>
-        <ProjectTile project_name="project8" />
+        <ProjectTile project_name="project8"
+        proj_image="https://via.placeholder.com/400x200.png"
+        />
       </li>
       <li>
-        <ProjectTile project_name="project9" />
+        <ProjectTile project_name="project9"
+        proj_image="https://via.placeholder.com/400x200.png"
+        />
       </li>
       <li>
-        <ProjectTile project_name="project10" />
+        <ProjectTile project_name="project10"
+        proj_image="https://via.placeholder.com/400x200.png"
+        />
       </li>
       <li>
-        <ProjectTile project_name="project11" />
+        <ProjectTile project_name="project11"
+        proj_image="https://via.placeholder.com/400x200.png"
+        />
       </li>
       <li>
-        <ProjectTile project_name="project12" />
+        <ProjectTile project_name="project12"
+        proj_image="https://via.placeholder.com/400x200.png"
+        />
       </li>
       <li>
-        <ProjectTile project_name="project13" />
+        <ProjectTile project_name="project13"
+        proj_image="https://via.placeholder.com/400x200.png"
+        />
       </li>
       <li>
-        <ProjectTile project_name="project14" />
+        <ProjectTile project_name="project14"
+        proj_image="https://via.placeholder.com/400x200.png"
+        />
       </li>
       <li>
-        <ProjectTile project_name="project15" />
+        <ProjectTile project_name="project15"
+        proj_image="https://via.placeholder.com/400x200.png"
+        />
       </li>
       <li>
-        <ProjectTile project_name="project16" />
+        <ProjectTile project_name="project16"
+        proj_image="https://via.placeholder.com/400x200.png"
+        />
       </li>
     </ul>
   </div>
