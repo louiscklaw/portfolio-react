15d10b70422afb7deabce0bc6a4dd3c8f082405e
louiscklaw <louiscklaw@gmail.com>
2019-11-19 20:03:58 +0800

    init commit, test ok,

--- src/components/NavTop.js
+++ src/components/NavTop.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
 import './NavTop.css'
+
+const element = <FontAwesomeIcon icon={faCoffee} />
 
 export default () => (
   <div className="nav-top">
     top
+    {element}
+
   </div>
 )
