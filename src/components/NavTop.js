0e63ded4ceaa07bba9120b1e8ef4467d00870203
louiscklaw <louiscklaw@gmail.com>
2019-11-22 00:42:57 +0800

    clean build erros,

--- src/components/NavTop.js
+++ src/components/NavTop.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import ReactDOM from 'react-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
