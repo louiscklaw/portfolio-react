c7429719c2e735e2a512ea8ff4cca0822178f2f5
louiscklaw <louiscklaw@gmail.com>
2019-11-21 23:45:17 +0800

    update to fix unused import Helloworld

--- src/routes/ProjectDetail.js
+++ src/routes/ProjectDetail.js
@@ -3,7 +3,6 @@ import React from 'react';
 import {Switch} from 'react-router-dom'
 import {Route} from 'react-router-dom'
 
-import Helloworld from '../pages/Helloworld'
 import ProjectDetail1 from '../pages/projects/Project1'
 
 
