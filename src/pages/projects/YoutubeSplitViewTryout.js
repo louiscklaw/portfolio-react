f12df9da79625f67d9f7967962025e5224209d80
louiscklaw <louiscklaw@gmail.com>
2019-11-23 19:31:05 +0800

    update,

--- src/pages/projects/YoutubeSplitViewTryout.js
+++ src/pages/projects/YoutubeSplitViewTryout.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import MetaTitle from '../../components/MetaTitle'
+
 import NewWindowLink from '../../components/Links/NewWindowLink'
 import GithubLink from '../../components/Links/GithubLink'
 
@@ -8,43 +10,52 @@ import ProjectTitle from '../../components/ProjectTitle'
 
 import YoutubeContainers from '../../components/containers/youtube'
 
+import MetaProjectDetail from '../../components/meta/ProjectDetail'
+
 import './ProjectDescription.css';
 import './YoutubeSplitViewTryout.css'
 
 import test_image from '../../assets/projects/youtube-split-view-tryout/youtube_split_view_tryout.png';
+import { MetaTags } from 'react-meta-tags';
 console.log(test_image);
 
 class ProjectDescription extends React.Component{
   render(){
     return (
-      <div className="desc-container">
-        <div className="project-description">
-          <BackToProjects />
+      <>
+        <MetaProjectDetail />
+        <MetaTitle text="youtube split view" />
 
-          <ProjectTitle project_title="youtube split view tryout" />
+        <div className="desc-container">
+          <div className="project-description">
+            <BackToProjects />
 
-          <div className="desc-header-picture" style={{display:"flex"}}>
-            <div>
-              <img src={test_image}  alt="" />
+            <ProjectTitle project_title="youtube split view tryout" />
+
+            <div className="desc-header-picture" style={{display:"flex"}}>
+              <div>
+                <img src={test_image} alt="" />
+              </div>
+              <div style={{maxWidth: "30%" }}>
+                <YoutubeContainers src="https://www.youtube.com/embed/YNCB23V38x0" />
+              </div>
             </div>
-            <div style={{maxWidth: "30%" }}>
-              <YoutubeContainers src="https://www.youtube.com/embed/YNCB23V38x0" />
+
+            <div className="desc-body">
+              <h3 className="topic">Purpose</h3>
+              <p>a simple page to monitor stock, while data provided by tradingview</p>
+
+              <h3 className="topic">Demo</h3>
+              <NewWindowLink link="https://louiscklaw.github.io/youtube-split-view-tryout/" />
+
+              <h3 className="topic">ref/repo:</h3>
+              <GithubLink link="https://github.com/louiscklaw/youtube-split-view-tryout" />
+
             </div>
-          </div>
-
-          <div className="desc-body">
-            <h3 className="topic">Purpose</h3>
-            <p>a simple page to monitor stock, while data provided by tradingview</p>
-
-            <h3 className="topic">Demo</h3>
-            <NewWindowLink link="https://louiscklaw.github.io/youtube-split-view-tryout/" />
-
-            <h3 className="topic">ref/repo:</h3>
-            <GithubLink link="https://github.com/louiscklaw/youtube-split-view-tryout" />
-
           </div>
         </div>
-      </div>
+      </>
+
     )
   }
 }
