c9ea40cbf01604b40b0ded83094db24d39002468
louiscklaw <louiscklaw@gmail.com>
2019-11-24 14:49:32 +0800

    update,

--- src/components/Links/GistLink.js
+++ src/components/Links/GistLink.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React,{PureComponent} from 'react';
 
-import ReactGist from 'react-gist';
+// import Gist from 'react-gist';
+import ReactEmbedGist from 'react-embed-gist';
 
-export default () => {
-  // let test="https://gist.github.com/louiscklaw/f9a49076aff09449a5e5e0078b30ce65"
-  // let gist_id = test.split('/').pop();
-  return(
-    <div>
-      {/* <ReactGist id="f9a49076aff09449a5e5e0078b30ce65" /> */}
-      <h1>helloworld</h1>
-    </div>
-
-  )
+class testGist extends React.PureComponent
+{
+  render() {
+    return (
+      <div>
+        <ReactEmbedGist gist={this.props.gist_id} />
+      </div>
+    )
+  }
 }
+
+export default testGist
+
+// export default () => {
+//   // let test="https://gist.github.com/louiscklaw/f9a49076aff09449a5e5e0078b30ce65"
+//   // let gist_id = test.split('/').pop();
+//   return(
+//     <div>
+//       <ReactGist id="f9a49076aff09449a5e5e0078b30ce65" />
+//       {/* <h1>helloworld</h1> */}
+//     </div>
+
+//   )
+// }
 
 // export default () => {
 //   return(<h1>helloworld</h1>)
--- src/tests/components/testGistLink.js
+++ src/tests/components/testGistLink.js
@@ -5,8 +5,7 @@ import GistLink from '../../components/Links/GistLink';
 
 it('renders helloworld call for gist without crashing', () => {
   const div = document.createElement('div');
-
-  let gist_link = <GistLink />;
+  let gist_link = <GistLink gist_id="louiscklaw/6a0206c038bbe82a969c1b7677713fb3" />;
 
   ReactDOM.render(gist_link, div);
   ReactDOM.unmountComponentAtNode(div);
