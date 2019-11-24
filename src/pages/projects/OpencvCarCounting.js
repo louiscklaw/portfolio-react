6f3e45e079d7889d90f58036360a5b18c51be015
louiscklaw <louiscklaw@gmail.com>
2019-11-24 17:34:57 +0800

    update topic,

--- src/pages/projects/OpencvCarCounting.js
+++ src/pages/projects/OpencvCarCounting.js
@@ -19,20 +19,18 @@ class ProjectDescription extends React.Component{
         <div className="project-description">
           <BackToProjects />
 
-          <ProjectTitle project_title="voron 3d printer" />
+          <ProjectTitle project_title="opencv car counting" />
 
           <div className="desc-header-picture" style={{display:"flex"}}>
             <div>
               <img src={head_pic}  alt="" />
             </div>
-            <div style={{maxWidth: "30%" }}>
-              <YoutubeContainers src="https://www.youtube.com/embed/8P3VEGvMeTQ" />
-            </div>
+
           </div>
 
           <div className="desc-body">
             <h3 className="topic">Purpose / description</h3>
-            <p>To count the number of cars passing by images only. This project is craeted on top of the another github works work.</p>
+            <p>To count the number of cars passing by images only. This project is created on top of the another github work.</p>
 
             <h3 className="topic">Requirements:</h3>
             <ul>
@@ -40,6 +38,11 @@ class ProjectDescription extends React.Component{
               <li>docker</li>
               <li>linux</li>
             </ul>
+
+            <h3 className="topic">Demo:</h3>
+            <div style={{maxWidth: "30%" }}>
+              <YoutubeContainers src="https://www.youtube.com/embed/8P3VEGvMeTQ" />
+            </div>
 
             <h3 className="topic">repository:</h3>
             <GithubLink link="https://github.com/louiscklaw/car_tracking_tryout" />
