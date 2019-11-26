5b3d4d6e62a69dd85a6fa2a8ce725cb36fe86404
louiscklaw <louiscklaw@gmail.com>
2019-11-26 18:36:27 +0800

    expand offline links,

--- src/custom-sw.js
+++ src/custom-sw.js
@@ -14,7 +14,25 @@ workbox.routing.registerRoute("/", workbox.strategies.networkFirst());
 
 // customize the path available for offline
 workbox.routing.registerRoute("/projects", workbox.strategies.networkFirst());
-workbox.routing.registerRoute("/project_detail/voron-tryout", workbox.strategies.networkFirst());
+
+
+// workbox.routing.registerRoute("/project_detail/voron-tryout", workbox.strategies.networkFirst());
+workbox.routing.registerRoute('/project_detail/tradingview-dashboard', workbox.strategies.NetworkFirst());
+workbox.routing.registerRoute('/project_detail/voron-tryout', workbox.strategies.NetworkFirst());
+workbox.routing.registerRoute('/project_detail/appium-behave-notes', workbox.strategies.NetworkFirst());
+workbox.routing.registerRoute('/project_detail/tappy', workbox.strategies.NetworkFirst());
+workbox.routing.registerRoute('/project_detail/portfolio', workbox.strategies.NetworkFirst());
+workbox.routing.registerRoute('/project_detail/youtube-split-view-tryout', workbox.strategies.NetworkFirst());
+workbox.routing.registerRoute('/project_detail/traefik-tryout', workbox.strategies.NetworkFirst());
+workbox.routing.registerRoute('/project_detail/pet-pug-project', workbox.strategies.NetworkFirst());
+workbox.routing.registerRoute('/project_detail/z-transfer-demo', workbox.strategies.NetworkFirst());
+workbox.routing.registerRoute('/project_detail/rthk31-rthk32-linux-viewer', workbox.strategies.NetworkFirst());
+workbox.routing.registerRoute('/project_detail/opencv-car-counting', workbox.strategies.NetworkFirst());
+workbox.routing.registerRoute('/project_detail/linux-toolbox', workbox.strategies.NetworkFirst());
+workbox.routing.registerRoute('/project_detail/hko-weather-bot', workbox.strategies.NetworkFirst());
+workbox.routing.registerRoute('/project_detail/slic3r-batch-export', workbox.strategies.NetworkFirst());
+workbox.routing.registerRoute('/project_detail/allwinner-v3s-tryout', workbox.strategies.NetworkFirst());
+workbox.routing.registerRoute('/project_detail/printer-board', workbox.strategies.NetworkFirst());
 
 // for (url_path of TileConfig){
 //   workbox.routing.registerRoute(
